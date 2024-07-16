45b52e51d03fbc1b23baeb18e50086817339cf27
MISRAIM HERNANDEZ <mxsrxm@gmail.com>
2024-07-15 18:31:31 -0600

    nuevos cambios

--- eventos/js/main.js
+++ eventos/js/main.js
@@ -1,3 +1,52 @@
+let notas = [
+        {
+            _id: 1,
+            titulo: 'Sacar la basura',
+            texto: 'Mi mama me va a regañar si no saco la basura',
+            realizada: false,
+        },
+        {
+            _id: 2,
+            titulo: 'Estudiar',
+            texto: 'Tengo que estudiar para el examen de mañana',
+            realizada: false,
+        },
+        {
+            _id: 3,
+            titulo: 'Hacer la tarea',
+            texto: 'Tengo que hacer la tarea de matemáticas',
+            realizada: true,
+        },
+        {
+            _id: 4,
+            titulo: 'Comprar el pan',
+            texto: 'No hay pan en la casa',
+            realizada: false,
+        },
+        {
+            _id: 5,
+            titulo: 'Llamar a mamá',
+            texto: 'Hace mucho que no hablo con mi mamá',
+            realizada: true,
+        },
+        {
+            _id: 6,
+            titulo: 'Hacer ejercicio',
+            texto: 'Hace mucho que no hago ejercicio',
+            realizada: true,
+        }
+    ];
+
+let data = {
+    notas: notas
+};
+
+
+data.notas
+data.notas[0].titulo
+
+let idGlobal = data.notas.length;
+
 function calcularIMC() {
     document.getElementById('form_calculadora_masa_corporal').addEventListener('submit', function (event) {
         event.preventDefault();
@@ -37,59 +86,14 @@ function convertidorMoneda() {
     });
 }
 
-
-const data = {
-    notas: [
-        {
-            _id: 1,
-            titulo: 'Sacar la basura',
-            texto: 'Mi mama me va a regañar si no saco la basura',
-            realizada: false,
-        },
-        {
-            _id: 2,
-            titulo: 'Estudiar',
-            texto: 'Tengo que estudiar para el examen de mañana',
-            realizada: false,
-        },
-        {
-            _id: 3,
-            titulo: 'Hacer la tarea',
-            texto: 'Tengo que hacer la tarea de matemáticas',
-            realizada: true,
-        },
-        {
-            _id: 4,
-            titulo: 'Comprar el pan',
-            texto: 'No hay pan en la casa',
-            realizada: false,
-        },
-        {
-            _id: 5,
-            titulo: 'Llamar a mamá',
-            texto: 'Hace mucho que no hablo con mi mamá',
-            realizada: true,
-        },
-        {
-            _id: 6,
-            titulo: 'Hacer ejercicio',
-            texto: 'Hace mucho que no hago ejercicio',
-            realizada: true,
-        },
-    ],
-};
-
-data.notas
-data.notas[0].titulo
-
 function mostrarNotas() {
     for (let i = 0; i < data.notas.length; i++) {
-        createCard(data.notas[i]);
+        crearCard(data.notas[i]);
 
     }
 }
 
-function createCard(nota) {
+function crearCard(nota) {
     let container_cards = document.getElementById("container_cards");
     let card = document.createElement("div");
     card.className = "card m-3";
@@ -106,27 +110,38 @@ function createCard(nota) {
     container_cards.appendChild(card);
 }
 
+function agregarNota(titulo, texto) {
+    idGlobal++; 
+    const nuevaNota = {
+        _id: idGlobal,
+        titulo: titulo,
+        texto: texto,
+        realizada: false,
+    };
+    data.notas.push(nuevaNota);
+}
 
+document.getElementById("form_tareas").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const titulo = document.getElementById("titulo").value;
+    const texto = document.getElementById("texto").value;
 
+    if (titulo && texto) {
+        agregarNota(titulo, texto); 
+        mostrarNotas(); 
+        document.getElementById("titulo").value = '';
+        document.getElementById("texto").value = '';
+    } else {
+        alert("Verifica que los campos estén llenos");
+    }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function borrarNota(id) {
+    data.notas = data.notas.filter(nota => nota._id !== id);
+    mostrarNotas();
+}
 
 
 calcularIMC();
 convertidorMoneda();
 mostrarNotas();
-
